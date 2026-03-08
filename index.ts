c9f11cd0e3e6417a87a7e8be4d2c4087a5ae2d16
Young Shung <me@ynshung.com>
2026-03-08 20:27:13 +0800

    fix: 2026 index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
+import { FY2026 } from "@/FY2026";
 import { FY2025 } from "./src/FY2025";
 
 export const years = {
   [FY2025.year]: FY2025,
+  [FY2026.year]: FY2026,
 };
 
 export type AvailableYears = keyof typeof years;
